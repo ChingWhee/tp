75de9a10719a1326fe38135a3a4ec826bf5d7816
ChingWhee <wongchingwhee@gmail.com>
2025-03-14 00:45:14 +0800

    Add ShoppingCatalogue, InventoryCatalogue, and RecipeCatalogue

---
+++ src/main/java/Catalogue/RecipeCatalogue.java
@@ -0,0 +1,12 @@
+package Catalogue;
+
+import backend.storage.Recipe;
+
+/**
+ * Manages the ingredients in a recipe
+ */
+public class RecipeCatalogue extends Catalogue<Recipe> {
+    public RecipeCatalogue() {
+        super();
+    }
+}
