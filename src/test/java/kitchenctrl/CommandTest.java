eef647c7da65b3985d02252c13f7c1287b4be7bb
Yee Zhan Xian Carlton <carltonyxz@gmail.com>
2025-03-15 23:11:27 +0800

    fixed for gradle again

--- src/test/java/kitchenctrl/CommandTest.java
+++ src/test/java/kitchenctrl/CommandTest.java
@@ -47,23 +47,5 @@ public class CommandTest {
         ArrayList<Ingredient> missingIngredients = Commands.getMissingIngredients(testInventory, recipe);
         assertTrue(missingIngredients.isEmpty(), "There should be enough ingredients");
     }
-/*
-    @Test
-    public void ingredientNotInInventory() {
-        IngredientCatalogue testInventory = new IngredientCatalogue();
-        Commands.addIngredient( testInventory, "Flour", 1000);
-        Commands.addIngredient( testInventory, "Eggs", 4);
-        // No Milk in inventory
 
-        Recipe recipe = new Recipe();
-        recipe.addItem(new Ingredient("Flour", 500));
-        recipe.addItem(new Ingredient("Eggs", 2));
-        recipe.addItem(new Ingredient("Milk", 300));
-
-        ArrayList<Ingredient> missingIngredients = Commands.getMissingIngredientsForRecipe(testInventory, recipe);
-        ArrayList<Ingredient> expectedMissing = new ArrayList<>();
-        expectedMissing.add(new Ingredient("Milk", 300));
-        assertEquals(missingIngredients, expectedMissing, "Should return True as 300 Milk is missing");
-    }
-*/
 }
