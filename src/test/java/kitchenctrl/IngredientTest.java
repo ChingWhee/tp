4485f8f2d3eb402a3217f5a34b2a3567e519be79
J030104 <B11030010@mail.ntust.edu.tw>
2025-03-20 20:07:57 +0800

    Follow coding standards

--- src/test/java/kitchenctrl/IngredientTest.java
+++ src/test/java/kitchenctrl/IngredientTest.java
@@ -2,7 +2,10 @@ package kitchenctrl;
 
 import model.Ingredient;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class IngredientTest {
 
