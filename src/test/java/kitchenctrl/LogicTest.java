adaea7e69ee7d74e0205afa6864f98c526386d63
Yee Zhan Xian Carlton <carltonyxz@gmail.com>
2025-03-30 19:06:13 +0800

    gradlaedkqpd

--- src/test/java/kitchenctrl/LogicTest.java
+++ src/test/java/kitchenctrl/LogicTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import static controller.ScreenState.RECIPE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 
 public class LogicTest {
