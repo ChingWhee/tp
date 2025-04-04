af3357dc31b7c4f46f5654527ba632e10e049c03
ChingWhee <wongchingwhee@gmail.com>
2025-04-04 16:22:11 +0800

    Update inventory to include quantity

--- src/main/java/model/catalogue/Inventory.java
+++ src/main/java/model/catalogue/Inventory.java
@@ -240,7 +240,7 @@ public class Inventory extends Catalogue<Ingredient> {
         }
         StringBuilder result = new StringBuilder("These are the items in your inventory:\n");
         for (int i = 0; i < items.size(); i++) {
-            String name = items.get(i).getIngredientName();
+            String name = items.get(i).toString();
             result.append(i + 1).append(". ").append(name == null ? "[Unnamed Ingredient]" : name).append("\n");
         }
         return new CommandResult(result.toString().trim());
